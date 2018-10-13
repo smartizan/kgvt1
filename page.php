b955a8e48da9bdac75abc891b107959cefddadd9
smartizan <mikead@yandex.ru>
2018-10-13 17:56:16 +0300

    bootstrap

--- page.php
+++ page.php
@@ -9,11 +9,27 @@
 
 get_header(); ?>
 
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-		<?php the_content(); ?>
-
-	<?php endwhile; ?>
-	<?php endif; ?>
+<div class="container-fluid">
+        <div class="row">
+                <div class="col-sm-8">
+                1 of 3
+                </div>
+                <div class="col-sm">
+                2 of 3 (wider)
+                </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-5">
+      2 of 3 (wider)
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
